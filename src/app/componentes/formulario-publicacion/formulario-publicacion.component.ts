e621400fdce31c2e2738fdb905872411ebfbbc20
rodrigo-munoz-vargas <rmparthoris@gmail.com>
2025-05-17 01:26:11 -0400

    componente camara agregado con botón para agregar foto funcional

--- src/app/componentes/formulario-publicacion/formulario-publicacion.component.ts
+++ src/app/componentes/formulario-publicacion/formulario-publicacion.component.ts
@@ -1,25 +1,42 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Publicacion } from 'src/app/modelo/publicacion';
-import { IonHeader, IonToolbar, IonButtons, IonTitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonInput, IonButton, IonBackButton, IonIcon, IonText } from "@ionic/angular/standalone";
+import { IonHeader, IonToolbar, IonButtons, IonTitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonInput, IonButton, IonBackButton, IonIcon, IonText, IonImg } from "@ionic/angular/standalone";
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-formulario-publicacion',
   templateUrl: './formulario-publicacion.component.html',
   styleUrls: ['./formulario-publicacion.component.scss'],
   standalone: true,
-  imports: [IonText, IonText, CommonModule, IonIcon, IonBackButton, IonHeader, IonToolbar, IonButtons, IonTitle, IonCard, IonCardHeader, IonButton, IonCardTitle, IonCardContent, IonItem, IonInput, IonButton, FormsModule]
+  imports: [IonImg, IonText, IonText, CommonModule, IonIcon, IonBackButton, IonHeader, IonToolbar, IonButtons, IonTitle, IonCard, IonCardHeader, IonButton, IonCardTitle, IonCardContent, IonItem, IonInput, IonButton, FormsModule]
 })
 export class FormularioPublicacionComponent {
   // Objeto que almacena los datos de la publicacion, vinculado con los campos del formulario
   publicacion: Publicacion = {
     descripcion: '',
-    titulo: ''
+    titulo: '',
+    foto: undefined
   };
 
   // Evento que permite comunicar al componente padre cuando se agrega una nueva publicacion
   @Output() publicacionAgregada = new EventEmitter<Publicacion>();
+
+  async tomarFoto() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+        source: CameraSource.Camera
+      });
+      
+      this.publicacion.foto = image.base64String;
+    } catch (error) {
+      console.error('Error al tomar foto:', error);
+    }
+  }
 
   constructor() { }
 
